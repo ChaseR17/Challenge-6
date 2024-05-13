160d4496606f4e8c1b2981ca02a9a6b16811ab95
Chase Gilhart <chasegilhart@Chases-MacBook-Pro.local>
2024-05-12 21:08:52 -0400

    Added new api details

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
 const APIKey = "a67e0ecb4838487f58850675d373057e"
 
+function cityInformation(cityName) {
+    let geoCodeUrl = "http://api.openweathermap.org/geo/1.0/direct?q="+cityName+"&limit=5&appid="+APIKey
+    fetch(geoCodeUrl)
+.then((res)=>{
+    return res.json()
+}).then((data)=>{
+    // console.log(data)
+    
+    // console.log(data[0].lat)
+    // console.log(data[0].lon)
+    let currentLat = data[0].lat
+    let currentLon = data[0].lon
+    getWeather(currentLat,currentLon)
+})
+}
+function getWeather (lat,lon) {
+    let weatherUrl = "http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid="+APIKey
+    fetch(weatherUrl)
+    .then((res)=>{
+        return res.json()
+    }).then((data)=>{
+        console.log(data.list)
+        let weatherArray = data.list
+        for (let i = 0; i < weatherArray.length; i+=8) {
+    console.log(weatherArray[i])
+            
+        }
+    })
+    }
+    cityInformation("new york")
+    
